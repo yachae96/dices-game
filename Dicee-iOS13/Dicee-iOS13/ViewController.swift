d94d5ce2cb610b4aaa90a53e19c57649d1c4507a
YACHAE <104hsh76@gmail.com>
2024-04-12 00:21:56 +0900

    버튼을 누르면 주사위 숫자가 바뀜

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 어디에서    무엇을  값
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceOne")
+    }
+
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceSix")
+    }
+    
+}
+
